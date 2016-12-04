707f773c45a956911685a6446243d128b52311cf
unsaidxpl <unsaidxpl@gmail.com>
2016-12-04 17:27:32 +0300

    Add database dump

---
+++ database/dump.sql
@@ -0,0 +1,15 @@
+-- Adminer 4.2.5 MySQL dump
+
+SET NAMES utf8;
+SET time_zone = '+00:00';
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `phone` varchar(255) NOT NULL,
+  `phone_key` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+-- 2016-12-04 14:03:06
